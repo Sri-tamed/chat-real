8f18c270e75abcead218367706d14366a77ca065
Gilson Oliveira <juninhokaponnemg@hotmail.com>
2021-07-25 18:32:51 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src='https://cdn.scaledrone.com/scaledrone.min.js'></script>
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Video Chat</title>
+</head>
+<body>
+<video id="localVideo" autoplay muted></video>
+<video id="remoteVideo" autoplay></video>
+
+<div class="footer">
+    <div class="icones">
+            <img src="./img/icons/off.png" alt="Finalizar Chamada" id="off"> 
+    </div> 
+</div>
+<script src="./js/index.js"></script>
+</body>
+</html>
